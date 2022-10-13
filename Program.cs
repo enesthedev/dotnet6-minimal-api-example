cd0ab12574850b3014338a19fa2e98b2a7acced8
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2022-10-13 21:05:47 +0300

    Add: Swagger Generic to builder services
    Add: OpenAPIInfo to Swagger Doc named as Pizza Store API

--- Program.cs
+++ Program.cs
@@ -1,5 +1,20 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo{Title="Pizza Store API", Description = "Making the Pizzas you love", Version = "v1"});
+});
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Pizza Store API v1");
+});
 
 app.MapGet("/", () => "Hello World!");
 
